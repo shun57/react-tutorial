bb216058869454a3cee03ba8243f73cd07729452
shun57 <tuktuk.032@gmail.com>
2022-05-17 00:51:40 +0900

    emotion(css-in-js)を追加
    コンポーネント修正
    スナップショットの更新

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import React from 'react';
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom';
 import Game from "./Game";
+import { Global, css } from '@emotion/react'
 
-ReactDOM.createRoot(document.getElementById("root")).render(<Game />);
+ReactDOM.render(
+    <React.StrictMode>
+        <Global
+            styles={css`
+                body {
+                    font: 14px "Century Gothic", Futura, sans-serif;
+                    margin: 20px;
+                }
+
+                ol,
+                ul {
+                    padding-left: 30px;
+                }
+            `}
+        />
+        <Game />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
